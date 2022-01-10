1d37863ecc57d234eab34ce76e09823fbbf71ed2
Keyboard Slayer <dalcjor@libmail.eu>
2022-01-10 21:28:58 +0100

    Clean old microkernel code

--- sources/libs/ansi/unistd.h
+++ sources/libs/ansi/unistd.h
@@ -27,38 +27,6 @@
 
 typedef uint64_t pid_t;
 
-enum UNISTD_SYSCALLS 
-{
-    SYS_EXIT,
-    SYS_LOG,
-    SYS_GETPID,
-    SYS_SENDIPC,
-    SYS_RECEIVEIPC,
-    SYS_ALLOC,
-    SYS_FREE,
-    SYS_REALLOC,
-    SYS_PLEDGE,
-    SYS_PLEDGE_IPC,
-    SYS_MEMCPY_IPC,
-    SYS_KILL,
-};
-
-enum IPC_ERROR
-{
-    IPC_OK,
-    IPC_PROCESS_NOT_PRESENT
-};
-
-typedef struct 
-{
-    pid_t pid_from;
-    pid_t pid_to;
-
-    char *name_to;
-    uint64_t command;
-    uint64_t data[IPC_DATA_LENGTH];
-} ipc_msg_t;
-
 #define __syscall_impl2(n, ...) syscall ## n(__VA_ARGS__)
 #define __syscall_impl(n, ...) __syscall_impl2(n, __VA_ARGS__)
 #define syscall(...) __syscall_impl(GET_ARG_COUNT(__VA_ARGS__), __VA_ARGS__)
@@ -68,14 +36,3 @@ uint64_t syscall2(uint64_t syscall_id, uint64_t arg1);
 uint64_t syscall3(uint64_t syscall_id, uint64_t arg1, uint64_t arg2);
 uint64_t syscall4(uint64_t syscall_id, uint64_t arg1, uint64_t arg2, uint64_t arg3);
 uint64_t syscall5(uint64_t syscall_id, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4);;
-
-void sys_log(const char *msg);
-void *sys_alloc(size_t amount);
-int pledge(char const *promises, char const *execpromises);
-int pledge_ipc(char const *promises, char const *execpromises);
-uint64_t ipc_send_msg(ipc_msg_t *msg);
-void ipc_receive_msg(ipc_msg_t *msg);
-void ipc_memcpy(uintptr_t dest, uintptr_t src, size_t length);
-
-pid_t getpid();
-int kill(pid_t pid);
